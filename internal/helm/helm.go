e12c211fe16316797bc4c51f91a88801e584cb07
Mitch Murphy <mitch.murphy@gmail.com>
2025-06-20 22:05:53 -0400

    feat(helm): refactor GetOptions method for cleaner option handling

--- internal/helm/helm.go
+++ internal/helm/helm.go
@@ -33,6 +33,26 @@ type Chart struct {
 	Version  string
 }
 
+// get Options
+func (c *Chart) GetOptions() []getter.Option {
+	var getterOpts []getter.Option
+
+	if c.Registry.Type == "HTTP" {
+		getterOpts = append(getterOpts, getter.WithPlainHTTP(true))
+	}
+	if c.Registry.Auth.Username != "" && c.Registry.Auth.Password != "" {
+		getterOpts = append(getterOpts, getter.WithBasicAuth(c.Registry.Auth.Username, c.Registry.Auth.Password))
+	}
+	if c.Registry.Insecure {
+		getterOpts = append(getterOpts, getter.WithInsecureSkipVerifyTLS(true))
+	}
+	if c.Registry.TLS.CertFile != "" && c.Registry.TLS.KeyFile != "" && c.Registry.TLS.CaFile != "" {
+		getterOpts = append(getterOpts, getter.WithTLSClientConfig(c.Registry.TLS.CertFile, c.Registry.TLS.KeyFile, c.Registry.TLS.CaFile))
+	}
+
+	return getterOpts
+}
+
 // ChartHasSchema checks if a chart has a values.schema.json file
 func (c *Chart) HasSchema() (bool, error) {
 	url := fmt.Sprintf("%s/%s-%s.tgz", c.Registry.URL, c.Name, c.Version)
@@ -56,23 +76,7 @@ func (c *Chart) HasSchema() (bool, error) {
 		return false, fmt.Errorf("unsupported registry type: %s", c.Registry.Type)
 	}
 
-	getterOpts := []getter.Option{}
-	if c.Registry.Type == "HTTP" {
-		getterOpts = append(getterOpts, getter.WithPlainHTTP(true))
-	}
-	if c.Registry.Auth.Username != "" && c.Registry.Auth.Password != "" {
-		getterOpts = append(getterOpts, getter.WithBasicAuth(c.Registry.Auth.Username, c.Registry.Auth.Password))
-	}
-	// if c.Registry.Auth.Token != "" {
-	// 	getterOpts = append(getterOpts, getter.WithToken(c.Registry.Auth.Token))
-	// }
-	if c.Registry.Insecure {
-		getterOpts = append(getterOpts, getter.WithInsecureSkipVerifyTLS(true))
-	}
-	if c.Registry.TLS.CertFile != "" && c.Registry.TLS.KeyFile != "" && c.Registry.TLS.CaFile != "" {
-		getterOpts = append(getterOpts, getter.WithTLSClientConfig(c.Registry.TLS.CertFile, c.Registry.TLS.KeyFile, c.Registry.TLS.CaFile))
-		getterOpts = append(getterOpts, getter.WithURL(url))
-	}
+	getterOpts := c.GetOptions()
 	provider, err := g.Get(url, getterOpts...)
 	if err != nil {
 		return false, fmt.Errorf("failed to get chart: %w", err)
@@ -93,4 +97,55 @@ func (c *Chart) HasSchema() (bool, error) {
 
 	log.Println("Chart does not have values.schema.json")
 	return false, nil
+}
+
+// GetSchema retrieves the values.schema.json file from the chart and saves to temporary file
+func (c *Chart) GetSchema() (string, error) {
+	hasSchema, err := c.HasSchema()
+	if err != nil {
+		return "", fmt.Errorf("error checking for schema: %w", err)
+	}
+	if !hasSchema {
+		return "", fmt.Errorf("chart does not have values.schema.json")
+	}
+
+	url := fmt.Sprintf("%s/%s-%s.tgz", c.Registry.URL, c.Name, c.Version)
+
+	// 1. Download the chart archive
+	var g getter.Getter
+	var e error
+	switch c.Registry.Type {
+	case "HTTP":
+		g, e = getter.NewHTTPGetter()
+		if e != nil {
+			return "", fmt.Errorf("failed to create HTTP getter: %w", e)
+		}
+	case "OCI":
+		g, e = getter.NewOCIGetter()
+		if e != nil {
+			return "", fmt.Errorf("failed to create OCI getter: %w", e)
+		}
+	default:
+		return "", fmt.Errorf("unsupported registry type: %s", c.Registry.Type)
+	}
+
+	getterOpts := c.GetOptions()
+	provider, err := g.Get(url, getterOpts...)
+	if err != nil {
+		return "", fmt.Errorf("failed to get chart: %w", err)
+	}
+
+	chart, err := loader.LoadArchive(provider)
+	if err != nil {
+		return "", fmt.Errorf("failed to load chart: %w", err)
+	}
+
+	for _, file := range chart.Files {
+		if file.Name == "values.schema.json" {
+			log.Println("Found values.schema.json in chart")
+			return string(file.Data), nil
+		}
+	}
+
+	return "", fmt.Errorf("values.schema.json not found in chart")
 }
